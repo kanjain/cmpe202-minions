c9d1ca31d1827612324214afba2a3367420dc088
Saurabh Ghotane <ghotanesg@gmail.com>
2017-11-01 00:27:05 -0700

    renaming the background sprite

--- setup/src/MainMenu.js
+++ setup/src/MainMenu.js
@@ -6,24 +6,25 @@ SaveTheMinions.MainMenu = function(game) {
 SaveTheMinions.MainMenu.prototype = {
 	create: function() {
 
-		this.game.add.sprite(0, 0, 'sjsu');
+	this.game.add.sprite(0, 0, 'background_city');
 
-	    // A button made from an image including text
-	    startButton = this.add.button(320, 200, 'playBtn', function(){this.game.state.start('Game')}, this, 1, 0, 2);
-	    startButton.input.useHandCursor = true;
-	    startButton.anchor.setTo(0.5,0.5);
+	// A button made from an image including text
+	startButton = this.add.button(320, 200, 'playBtn', function(){this.game.state.start('Game')}, this, 1, 0, 2);
+	startButton.input.useHandCursor = true;
+	startButton.anchor.setTo(0.5,0.5);
 
-	    // A button made from an image including text
-	    howToButton = this.add.button(320, 300, 'howToBtn', function(){this.game.state.start('StoryHowto')}, this, 1, 0, 2);
-	    howToButton.anchor.setTo(0.5,0.5);
+	// A button made from an image including text
+	howToButton = this.add.button(320, 300, 'howToBtn', function(){this.game.state.start('StoryHowto')}, this, 1, 0, 2);
+	howToButton.anchor.setTo(0.5,0.5);
 
-	    // A button made from an image to select levels
+	// A button made from an image to select levels
 
-        howToButton = this.add.button(320, 400, 'levelBtn', function(){this.game.state.start('LevelSelect')}, this, 1, 0, 2);
-        howToButton.anchor.setTo(0.5,0.5);
+	howToButton = this.add.button(320, 400, 'levelBtn', function(){this.game.state.start('LevelSelect')}, this, 1, 0, 2);
+	howToButton.anchor.setTo(0.5,0.5);
 
-	    // This property is needed to display Hand cursor everytime. By default it only displays it once before being clicked. After that normal arrow head cursor is displayed.
-	    howToButton.input.useHandCursor = true;
+	// This property is needed to display Hand cursor everytime. By default it only displays it once before being clicked. 
+	// After that normal arrow head cursor is displayed.
+	howToButton.input.useHandCursor = true;
 
 	}
 };
