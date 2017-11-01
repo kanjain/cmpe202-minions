5131f1748b682ae8b9432ba6902f0da9dc9fcd5f
Saurabh Ghotane <ghotanesg@gmail.com>
2017-11-01 00:28:21 -0700

    adding a new back button to go back to main menu

--- setup/src/StoryHowto.js
+++ setup/src/StoryHowto.js
@@ -7,7 +7,7 @@ SaveTheMinions.StoryHowto.prototype = {
 		this.showStory();
 	},
 	showStory: function() {
-		this.game.add.sprite(0, 0, 'sjsu');
+		this.game.add.sprite(0, 0, 'background_city');
 		this.game.add.sprite(100, 100, 'story');
 		//this.game.add.sprite(0, 0, 'screen-story');
 		this.state = 'story';
@@ -15,5 +15,9 @@ SaveTheMinions.StoryHowto.prototype = {
 		startButton = this.add.button(320, 400, 'playBtn', function(){this.game.state.start('Game')}, this, 1, 0, 2);
 		startButton.input.useHandCursor = true;
 		startButton.anchor.setTo(0.5,0.5);
+
+		backButton = this.add.button(320, 500, 'backBtn', function(){this.game.state.start('MainMenu')}, this, 1, 0, 2);
+		backButton.input.useHandCursor = true;
+		backButton.anchor.setTo(0.5,0.5);
 	}
 };
