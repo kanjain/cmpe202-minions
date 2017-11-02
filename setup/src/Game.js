1941e4226ef8f52d621755d091dc321710d7ffda
Kanika Jain <kanikajain.thakkar@gmail.com>
2017-11-01 22:26:19 -0700

    Refactor code and name variables properly

--- setup/src/Game.js
+++ setup/src/Game.js
@@ -1,10 +1,12 @@
 SaveTheMinions.Game = function(game) {
 	cTime = 0;
-    count = 0;
-	count1 = 0;
-	test = 0;
+    healthScore = 0;
+	totalMinions = 0;
+	score = 0;
 	scoreIncrement =1;
 	consecutiveCount=0;
+    score = 0;
+    health = 0;
 
 	transportation = null;
 	minionArray = ['Dave','Tim','Jerry'];
@@ -15,41 +17,41 @@ SaveTheMinions.Game = function(game) {
 SaveTheMinions.Game.prototype = {
 	create: function() {
 
-			// physic global setup
-		    this.game.physics.startSystem(Phaser.Physics.ARCANE);
-		    this.game.physics.arcade.gravity.y = 500;
+        // physic global setup
+        this.game.physics.startSystem(Phaser.Physics.ARCANE);
+        this.game.physics.arcade.gravity.y = 500;
 
-		    //  Background image for our game.
-				if (this.game.theme == "forest"){
-					this.game.add.sprite(0, 0, 'background_forest');
-					// Adding a new transportation object to save the minions.
-		    		transportation = this.game.add.sprite(100, 608, 'truck');
-				}
-				else if (this.game.theme == "city"){
-					this.game.add.sprite(0, 0, 'background_city');
-					// Adding a new transportation object to save the minions.
-		    		transportation = this.game.add.sprite(100, 608, 'truck');
-				}
-					
-				else if (this.game.theme == "space"){
-					this.game.add.sprite(0, 0, 'background_space');
-					// Adding a new transportation object to save the minions.
-		    		transportation = this.game.add.sprite(60, 628, 'spaceship');
-				}
-				else {
-					this.game.add.sprite(0, 0, 'background_city');
-					// Adding a new transportation object to save the minions.
-		    		transportation = this.game.add.sprite(100, 608, 'truck');
+        //  Background image for our game.
+            if (this.game.theme == "forest"){
+                this.game.add.sprite(0, 0, 'background_forest');
+                // Adding a new transportation object to save the minions.
+                transportation = this.game.add.sprite(100, 608, 'truck');
+            }
+            else if (this.game.theme == "city"){
+                this.game.add.sprite(0, 0, 'background_city');
+                // Adding a new transportation object to save the minions.
+                transportation = this.game.add.sprite(100, 608, 'truck');
+            }
 
-				}
+            else if (this.game.theme == "space"){
+                this.game.add.sprite(0, 0, 'background_space');
+                // Adding a new transportation object to save the minions.
+                transportation = this.game.add.sprite(60, 628, 'spaceship');
+            }
+            else {
+                this.game.add.sprite(0, 0, 'background_city');
+                // Adding a new transportation object to save the minions.
+                transportation = this.game.add.sprite(100, 608, 'truck');
 
-			transportation.anchor.setTo(0.5,0.5);
-		    // add group
-			flyingMinions = this.game.add.group();
+            }
 
-			// instantiate the observer
-			onScoreChange = new Observer();
+        transportation.anchor.setTo(0.5,0.5);
+        // add group
+        flyingMinions = this.game.add.group();
 
+        // instantiate the observer
+        onScoreChange = new Observer();
+        // subscribe to a subject
         onScoreChange.subscribe(this.updateScore);
 		},
 	update: function() {
@@ -81,7 +83,7 @@ SaveTheMinions.Game.prototype = {
 	            minion.body.collideWorldBounds = false;
 	            minion.body.velocity.y = this.game.rnd.realInRange(-300.0, -500.0);
 	            minion.body.velocity.x = this.game.rnd.realInRange(150.0, 200.0);
-	            count1++;
+	            totalMinions++;
 
 	        }
 	        // same logic as the block above but for right corner
@@ -106,7 +108,7 @@ SaveTheMinions.Game.prototype = {
 	            minion.body.collideWorldBounds = false;
 	            minion.body.velocity.y = this.game.rnd.realInRange(-300.0, -500.0);
 	            minion.body.velocity.x = this.game.rnd.realInRange(-150.0, -200.0);
-	            count1++;
+	            totalMinions++;
 	        }
 
 	    }
@@ -117,18 +119,17 @@ SaveTheMinions.Game.prototype = {
 	            sprite.destroy();
                 consecutiveCount = 0;
                 scoreIncrement =1;
-	            count++;
+	            healthScore++;
 			}
 
 	    });
 	},
 	render: function() {
-		this.game.debug.text('dead minions: ' + count, 100, 100);
-		this.game.debug.text('total number of minions: ' + count1, 100, 120);
+		this.game.debug.text('dead minions: ' + healthScore, 100, 100);
+		this.game.debug.text('total number of minions: ' + totalMinions, 100, 120);
 		this.decorateScore();
-        this.game.debug.text('saved minion: ' + test, 100, 140);
+        this.game.debug.text('saved minion: ' + score, 100, 140);
 	},
-
     decorateScore: function() {
     if(consecutiveCount> 2 && consecutiveCount < 4 ){
         scoreIncrement = 2 * scoreIncrement;
@@ -137,19 +138,15 @@ SaveTheMinions.Game.prototype = {
             scoreIncrement = 2 * scoreIncrement;
         }
     },
-
 	updateScore: function(event) {
 		if (event === 'addOne') {
-			test += scoreIncrement;
+			score += scoreIncrement;
 			consecutiveCount+=1;
 		}
 	},
-
 	selectt: function(sprite){
 		this.game.physics.arcade.moveToObject(sprite, transportation, 0, 50);
 		sprite.lifespan = 55;
 		onScoreChange.notify('addOne');
 	}
-
-
 };
--- setup/src/Preloader.js
+++ setup/src/Preloader.js
@@ -5,15 +5,16 @@ SaveTheMinions.Preloader = function(game) {
 SaveTheMinions.Preloader.prototype = {
 	preload: function() {
 	this.game.stage.backgroundColor = '#000000';
+
 	this.preloadBar = this.add.sprite((640-311)/2, (960-27)/2, 'preloaderBar');
 	this.load.setPreloadSprite(this.preloadBar);
 
-        this.game.load.image('background_city', 'img/min2.png');
-        this.game.load.image('background_forest', 'img/background_forest.jpg');
-        this.game.load.image('background_space', 'img/background_space.jpg');
+    this.game.load.image('background_city', 'img/min2.png');
+    this.game.load.image('background_forest', 'img/background_forest.jpg');
+    this.game.load.image('background_space', 'img/background_space.jpg');
 
-        this.game.load.image('levelBtn', 'img/btn_select_level.png');
-        this.game.load.image('backBtn', 'img/btn_back.png');
+    this.game.load.image('levelBtn', 'img/btn_select_level.png');
+    this.game.load.image('backBtn', 'img/btn_back.png');
 	this.game.load.image('playBtn', 'img/btn_play.png');
 	this.game.load.image('moderate', 'img/btn_moderate.png');
 	this.game.load.image('hard', 'img/btn_hard.png');
