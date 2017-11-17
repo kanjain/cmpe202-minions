7afb359b253f0d8e7759f4d1c94818b7e8f0a6a2
Kanika Jain <kanikajain.thakkar@gmail.com>
2017-11-16 22:41:39 -0800

    Add end of game screen with score and try again button

--- setup/src/End.js
+++ setup/src/End.js
@@ -4,23 +4,25 @@ SaveTheMinions.EndOfGame = function(game) {
 };
 SaveTheMinions.EndOfGame.prototype = {
 	create: function() {
-		this.showStory();
+		this.showEndScreen();
 	},
-	showStory: function() {
-		this.game.add.sprite(0, 0, 'background_city');
-		//this.game.add.sprite(100, 100, 'story');
-		//this.game.add.sprite(0, 0, 'screen-story');
-		this.state = 'story';
+	showEndScreen: function() {
+	    var score = this.game.displayscore;
+		var background = this.game.add.sprite(0, 0, 'minion_end_game');
+		var gameOverImage = this.game.add.sprite(650,75,'minion_game_over');
+		var health = 25;
 
-		var style = { font: "75px ComicBook", fill: "#ff0044", align: "center" };
-    var text = this.game.add.text(470, 250, "You Lost! \n Your score \n"+this.game.displayscore, style);
-    text.anchor.set(0.5);
+		background.tint = 0xF234234;
+        this.game.add.sprite(10, 10, 'score-bg');
+        this.game.add.sprite(13, 13, 'score-bg-minion-icon');
+        scoreText = this.game.add.text(150, 20, "0", { font: "40px ComicBook", fill: "#FFCC00", align: "right" });
+        scoreText.setText(score);
 
-		startButton = this.add.button(320, 400, 'playBtn', function(){this.game.state.start('Game')}, this, 1, 0, 2);
-		startButton.input.useHandCursor = true;
-		startButton.anchor.setTo(0.5,0.5);
+		tryAgainButton = this.add.button(450, 350, 'restartButton', function(){this.game.state.start('Game');}, this, 1, 0, 2);
+		tryAgainButton.input.useHandCursor = true;
+		tryAgainButton.anchor.setTo(0.5,0.5);
 
-		backButton = this.add.button(320, 500, 'backBtn', function(){this.game.state.start('MainMenu')}, this, 1, 0, 2);
+		backButton = this.add.button(1100, 350, 'backToMainButton', function(){this.game.state.start('MainMenu');}, this, 1, 0, 2);
 		backButton.input.useHandCursor = true;
 		backButton.anchor.setTo(0.5,0.5);
 	}
